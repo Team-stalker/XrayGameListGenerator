7ba93f362f908421b5869e91c0748cc91a774d6f
Team-stalker <39802486+Team-stalker@users.noreply.github.com>
2025-02-09 21:29:25 +0500

    Add files via upload

---
+++ XrayGameListGenerator/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace XrayGameListGenerator
+{
+    partial class XrayGameListFileGenerator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XrayGameListFileGenerator));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFileOut = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPathMods = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ссылка на папку с файлами на FTP:";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxURL.Location = new System.Drawing.Point(11, 45);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(338, 20);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.Text = "https://team-stalker.ucoz.com/forum/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Имя файла (gamelist.txt) на выходе:";
+            // 
+            // textBoxFileOut
+            // 
+            this.textBoxFileOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxFileOut.Location = new System.Drawing.Point(11, 123);
+            this.textBoxFileOut.Name = "textBoxFileOut";
+            this.textBoxFileOut.Size = new System.Drawing.Size(338, 20);
+            this.textBoxFileOut.TabIndex = 3;
+            this.textBoxFileOut.Text = "gamelist";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(11, 145);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(339, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(362, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О Программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Добавить к пути (path=):";
+            // 
+            // textBoxPathMods
+            // 
+            this.textBoxPathMods.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPathMods.Location = new System.Drawing.Point(11, 83);
+            this.textBoxPathMods.Name = "textBoxPathMods";
+            this.textBoxPathMods.Size = new System.Drawing.Size(338, 20);
+            this.textBoxPathMods.TabIndex = 7;
+            this.textBoxPathMods.Text = "mods\\";
+            // 
+            // XrayGameListFileGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 179);
+            this.Controls.Add(this.textBoxPathMods);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBoxFileOut);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(378, 218);
+            this.MinimumSize = new System.Drawing.Size(378, 218);
+            this.Name = "XrayGameListFileGenerator";
+            this.Text = "XrayGameListFileGenerator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFileOut;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPathMods;
+    }
+}
+
---
+++ XrayGameListGenerator/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XrayGameListGenerator
+{
+    public partial class XrayGameListFileGenerator : Form
+    {
+        public XrayGameListFileGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void Init()
+        {
+            string folderPath = @"files"; 
+
+            string baseUrl = textBoxURL.Text;
+
+            string outputFile = $"{textBoxFileOut.Text}.txt";
+
+
+            string[] files = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories);
+
+            using (StreamWriter writer = new StreamWriter(outputFile, false, Encoding.UTF8))
+            {
+                writer.WriteLine("[main]");
+                writer.WriteLine("files_count=" + files.Length);
+                writer.WriteLine();
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string fileFullPath = files[i];
+                    string relativePath = GetRelativePath(folderPath, fileFullPath);
+                    long size = new FileInfo(fileFullPath).Length;
+                    string crc32 = ComputeCRC32(fileFullPath);
+                    string url = baseUrl + relativePath.Replace("\\", "/");
+
+                    writer.WriteLine("[file_" + i + "]");
+                    writer.WriteLine("path=" + textBoxPathMods.Text + relativePath);
+                    writer.WriteLine("url=" + url);
+                    writer.WriteLine("size=" + size);
+                    writer.WriteLine("crc32=" + crc32);
+                    writer.WriteLine();
+                }
+            }
+            MessageBox.Show("Обработка файлов завершена. Файл находится рядом с программой" + outputFile, "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private static string GetRelativePath(string basePath, string fullPath)
+        {
+            basePath = Path.GetFullPath(basePath);
+            fullPath = Path.GetFullPath(fullPath);
+
+            if (!basePath.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                basePath += Path.DirectorySeparatorChar;
+            }
+            Uri baseUri = new Uri(basePath);
+            Uri fullUri = new Uri(fullPath);
+            Uri relativeUri = baseUri.MakeRelativeUri(fullUri);
+
+            return Uri.UnescapeDataString(relativeUri.ToString()).Replace('/', Path.DirectorySeparatorChar);
+        }
+
+
+        static string ComputeCRC32(string filePath)
+        {
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var crc32 = new XrayCrc32();
+                byte[] hash = crc32.ComputeHash(fs);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработано командой: Team-stalker\n\n\rПрограмма предназначена для автоматического формирования файла gamelist.txt.\n\n\rФайлы мода необходимо поместить в папку files. После чего, будет выполнена обработка.");
+        }
+    }
+
+    public class XrayCrc32 : HashAlgorithm
+    {
+        public override int HashSize => 32;
+
+        public const uint DefaultPolynomial = 0xEDB88320u;
+        public const uint DefaultSeed = 0xFFFFFFFFu;
+
+        private uint hash;
+        private uint seed;
+        private uint[] table;
+
+        public XrayCrc32()
+        {
+            table = InitializeTable(DefaultPolynomial);
+            seed = DefaultSeed;
+            Initialize();
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] buffer, int start, int length)
+        {
+            hash = CalculateHash(table, hash, buffer, start, length);
+        }
+
+        protected override byte[] HashFinal()
+        {
+            byte[] hashBuffer = BitConverter.GetBytes(~hash);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(hashBuffer);
+            return hashBuffer;
+        }
+
+        private static uint[] InitializeTable(uint polynomial)
+        {
+            uint[] table = new uint[256];
+            for (uint i = 0; i < 256; i++)
+            {
+                uint entry = i;
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry >>= 1;
+                }
+                table[i] = entry;
+            }
+            return table;
+        }
+
+        private static uint CalculateHash(uint[] table, uint seed, byte[] buffer, int start, int length)
+        {
+            uint crc = seed;
+            for (int i = start; i < start + length; i++)
+            {
+                crc = (crc >> 8) ^ table[buffer[i] ^ (crc & 0xFF)];
+            }
+            return crc;
+        }
+    }
+}
